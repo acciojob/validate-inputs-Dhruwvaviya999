0fabb86ebcf19e9c6c8d8682d1188ca77bec7c6a
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-07-10 17:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+
+let inputs = document.getElementsByTagName("input");
+inputs = [...inputs]
+inputs.forEach((input) => {
+	input.addEventListener("input", (e) => {
+		const pattern = new RegExp(input.getAttribute("pattern"));
+		const value = input.value;
+
+		if(pattern.test(value)){
+			input.classList.add("valid");
+			input.classList.remove("in-valid");
+		} else {
+			input.classList.add("in-valid");
+			input.classList.remove("valid")
+		}
+	})
+;})
+
